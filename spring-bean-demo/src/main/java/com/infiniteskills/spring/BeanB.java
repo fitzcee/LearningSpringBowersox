ff7af89bc2e8135016ec5c5b2690710228e07713
fitzcee-pc <fitzcee-pc@users.noreply.github.com>
2016-07-19 19:02:01 -0700

    completed the c-namespace and p-namespace examples

--- spring-bean-demo/src/main/java/com/infiniteskills/spring/BeanB.java
+++ spring-bean-demo/src/main/java/com/infiniteskills/spring/BeanB.java
@@ -2,6 +2,14 @@ package com.infiniteskills.spring;
 
 public class BeanB {
 
+	private String val;
+	
+	public BeanB(String val) {
+		super();
+		this.val = val;
+		System.out.println(String.format("In Bean B string constructor, with val = %1s", this.val));
+	}
+
 	public void execute() {
 		System.out.println("In Bean B execute");
 	}
